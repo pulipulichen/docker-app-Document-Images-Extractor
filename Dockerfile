3d4d9d4e51b71f26eac3cfe2d83511cd8f3fa70c
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-11-25 17:30:50 +0800

    西元2023年11月25日 (週六) 17時30分50秒 CST

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM pudding/docker-app:node-18-7z-20230521
+
+RUN apt-get update --fix-missing
+
+RUN apt-get install -y \
+    poppler-utils
+
+RUN apt-get install -y \
+    unzip
+
+# COPY package.json /
+# RUN npm install
+
+#CMD ["bash"]
---
+++ app/index.js
@@ -0,0 +1,91 @@
+const ShellExec = require('./lib/ShellExec')
+const GetFiles = require('./lib/GetFiles')
+
+const path = require('path')
+const fs = require('fs')
+
+let main = async function () {
+  let files = GetFiles()
+
+  for (let i = 0; i < files.length; i++) {
+    let file = files[i]
+    if (file.endsWith('.pdf')) {
+      await processSinglePDF(file)
+    }
+    else if (file.endsWith('.odt') || file.endsWith('.ods') || file.endsWith('.odp') || file.endsWith('.odg')) {
+      await processSingleODF(file)
+    }
+  }
+}
+
+let processSinglePDF = async function (file) {
+  let filename = path.basename(file)
+  // let dirname = path.dirname(file)
+  let filenameNoExt = filename
+  if (filenameNoExt.endsWith('.pdf')) {
+    filenameNoExt = filenameNoExt.slice(0, -4)
+  }
+
+  let outputFolder = `/output/${filenameNoExt}/`
+  fs.mkdirSync(outputFolder, {recursive: true})
+
+  let result
+  let cmd = `pdfimages "${file}" -png "${outputFolder}"`
+  console.log(cmd)
+  try {
+    result = await ShellExec(cmd)
+  }
+  catch (e) {
+    console.error(e)
+  }
+
+  prependFilenameInFolder(filenameNoExt, outputFolder)
+}
+
+let processSingleODF = async function (file) {
+  let filename = path.basename(file)
+  // let dirname = path.dirname(file)
+  let filenameNoExt = filename
+  if (filenameNoExt.slice(-4, -3) === '.') {
+    filenameNoExt = filenameNoExt.slice(0, -4)
+  }
+
+  let outputFolder = `/output/${filenameNoExt}/`
+  fs.mkdirSync(outputFolder, {recursive: true})
+
+  let result
+  
+  let cmd = `unzip "${file}" -d "${outputFolder}"`
+  console.log(cmd)
+  try {
+    result = await ShellExec(cmd)
+  }
+  catch (e) {
+    console.error(e)
+  }
+
+  prependFilenameInFolder(filenameNoExt, outputFolder)
+}
+
+let prependFilenameInFolder = function (prepend, directoryPath) {
+  try {
+    const files = fs.readdirSync(directoryPath);
+  
+    files.forEach((file) => {
+      const oldFilePath = path.join(directoryPath, file);
+      const newFileName = prepend + ` - ${file}`;
+      const newFilePath = path.join(directoryPath, newFileName);
+  
+      try {
+        fs.renameSync(oldFilePath, newFilePath);
+        console.log(`Renamed ${file} to ${newFileName}`);
+      } catch (renameError) {
+        console.error(`Error renaming file ${file}:`, renameError);
+      }
+    });
+  } catch (readDirError) {
+    console.error('Error reading directory:', readDirError);
+  }
+}
+
+main()
